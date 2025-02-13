a81ae25371d8e9b7b62d0703a63900828debd1eb
MatheusGmota <livematgreg@gmail.com>
2025-02-13 19:16:42 -0300

    Nivelamento Javascript: criando variáveis e imprimindo elas no console, explicando a diferença entre null e undefined

---
+++ index.js
@@ -0,0 +1,13 @@
+// Variáveis e Tipos de Dados  
+// Exercício 1:  
+const nome = 'Matheus'
+let idade = 19
+let aprovado = true
+let notas = [8,7,9]
+let usuario = { email: "theus@email.com", senha: "12345"}
+
+console.log("Nome: " + nome + " - Idade: " + idade + " - Aprovado: " + aprovado + " - Notas: " + notas + " - Email: " + usuario.email + " - Senha: " + usuario.senha )
+
+// Diferença entre null e undefined
+// O 'null' é usado em casos em que uma variável é iniciada propositalmente com valor nulo, indicando estar vazia
+// O 'undefined' ocorre em casos em que uma variável é declarada e não recebeu uma atribuição ou valor  
