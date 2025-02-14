e0c41e164fc83aa5eb6aca018226ed13300ffebf
MatheusGmota <livematgreg@gmail.com>
2025-02-13 22:11:45 -0300

    Exercício 1 de Funções e Escopo finalizado

--- exercicio-5.js
+++ exercicio-5.js
@@ -1,10 +1,13 @@
 let texto = "ola"
 
-console.log("Transformando em maiúsculas");
+// console.log("Transformando em maiúsculas");
 
 converterParaMaiusculas = (string) => {
-    return string.toUpperCase() 
+    return string.toUpperCase();
+    let mensagem = "Transformando em maiúsculas"
 } 
+
+console.log(mensagem); // Não é possível acessar a variável 'mensagem' por que ela foi definida e atribuída dentro do escopo da função
 
 console.log("Texto em maiúsculas:", converterParaMaiusculas(texto));
 
