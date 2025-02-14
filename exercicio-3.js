1cc7d6b84a2cc79fbd6ca3ba42fe6d4bd586553c
MatheusGmota <livematgreg@gmail.com>
2025-02-13 21:44:31 -0300

    Nivelamento Javascript: criando condicional para verificar se o numero é positivo ou negativo ou zero

---
+++ exercicio-3.js
@@ -0,0 +1,12 @@
+let numero = -1
+
+if (numero === 0) {
+    console.log("É zero");
+    
+} else if (numero > 0) {
+    console.log("Número é positvo");
+    
+} else {
+    console.log("Número é negativo");
+    
+}
