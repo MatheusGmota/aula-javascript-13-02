e75984d66e96cf2624894bbef62c1a360c6ce537
MatheusGmota <livematgreg@gmail.com>
2025-02-13 21:39:24 -0300

    Nivelamento Javascript: criando duas variaveis e verificando se é nula ou undefined

---
+++ exercicio-2.js
@@ -0,0 +1,15 @@
+// Exercício 2
+let variavel1 = null
+let variavel2
+
+if (variavel1 === null) {
+    console.log("Variável é nula.");
+} else {
+    console.log("Variável é undefined.");
+}
+
+if (variavel2 === undefined) {
+    console.log("Variável é undefined.");
+} else {
+    console.log("Variável é nula.");
+}
