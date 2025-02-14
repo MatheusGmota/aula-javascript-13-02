2f39212b7baea96d9579f100d0e7aaa67e447e4f
MatheusGmota <livematgreg@gmail.com>
2025-02-13 22:01:51 -0300

    Crie um sistema de menu usando switch-case e do-while

---
+++ exercicio-4.js
@@ -0,0 +1,17 @@
+let opcao = null // implementar: receber input do usuario
+
+do {
+    switch (opcao) {
+        case "A":
+            console.log("Opção A selecionada");
+            break
+        case "B":
+            console.log("Opção B selecionada");
+            break
+        case "C":
+            console.log("Opção C selecionada");
+            break
+        default: 
+            console.log("Opção inválida");   
+    }
+} while (opcao !== "S") 
