61a421c1822d30b4988c9bab55e3cfc183233357
MatheusGmota <livematgreg@gmail.com>
2025-02-13 22:06:58 -0300

    Criando função para converter uma string em maiúsculas

---
+++ exercicio-5.js
@@ -0,0 +1,10 @@
+let texto = "ola"
+
+console.log("Transformando em maiúsculas");
+
+function converterParaMaiusculas(string) {
+    return string.toUpperCase() 
+} 
+
+console.log("Texto em maiúsculas:", converterParaMaiusculas(texto));
+
